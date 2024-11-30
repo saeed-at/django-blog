fc1ed42b491db44c720fb0702c5ad27db5d0a82c
saeed.at <ssaeed.alijani@aut.ac.ir>
2024-12-01 01:58:35 +0330

    add basic search

--- djblogger/djblogger/blog/views.py
+++ djblogger/djblogger/blog/views.py
@@ -3,6 +3,7 @@ from django.db.models.query import QuerySet
 from .models import Post
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
+from .forms import PostSearchForm
 
 
 class HomeView(ListView):
@@ -40,3 +41,21 @@ class TagListView(ListView):
         context = super(TagListView, self).get_context_data(**kwargs)
         context["tag"] = self.kwargs["tag"]
         return context
+
+
+class PostSearchView(ListView):
+    model = Post
+    paginate_by = 5
+    context_object_name = "posts"
+    form_class = PostSearchForm
+
+    def get_template_names(self):
+        if self.request.htmx:
+            return "blog/components/post-list-elements-search.html"
+        return "blog/search.html"
+
+    def get_queryset(self):
+        form = self.form_class(self.request.GET)
+        if form.is_valid():
+            return Post.objects.filter(title__icontains=form.cleaned_data["q"])
+        return []
